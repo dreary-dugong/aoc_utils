b866e0187c869a393e156c88321fc42b250f3bf7
Daniel Gysi <danielgysi@protonmail.com>
2023-11-15 18:26:15 -0500

    [aocex] fix run call

--- aocex/src/main.rs
+++ aocex/src/main.rs
@@ -1,7 +1,7 @@
 use aocex::Config;
 use std::process;
 fn main() {
-    if let Err(e) = run(Config::make()) {
+    if let Err(e) = aocex::run(Config::make()) {
         eprintln!("{}", e);
         process::exit(1);
     } else {
